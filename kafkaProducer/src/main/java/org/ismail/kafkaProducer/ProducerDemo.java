c28464f6edd9466a34ebbe3981f6b6fcab91d85f
İsmail Çolak <ismail.colak@ogr.iuc.edu.tr>
2025-09-04 10:58:57 +0300

    producer büyük dosya gönderimi (parçalama)

--- kafkaProducer/src/main/java/org/ismail/kafkaProducer/ProducerDemo.java
+++ kafkaProducer/src/main/java/org/ismail/kafkaProducer/ProducerDemo.java
@@ -15,6 +15,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.UUID;
 
 import static org.ismail.kafkaProducer.utils.Utilities.getFileType;
 
@@ -45,21 +47,38 @@ public class ProducerDemo {
     public void sendFileMessage(String topicName, String filePath) throws IOException {
         File file = new File(filePath);
         byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
+        System.out.println(fileBytes.length);
         String mimeType = getFileType(filePath);
+        String fileId = UUID.randomUUID().toString();
 
-        //MyMessage bigMessage = new MyMessage(file.getName(),LocalDateTime.now(),mimeType,filePath,"sasl-producer",topicName);
+        int totalChunk = (int) Math.ceil((double)fileBytes.length / MyMessage.chunkSize);
+        System.out.println(totalChunk);
+        for(int i = 0; i < totalChunk; i++) {
+            int start = i * MyMessage.chunkSize;
+            int end = Math.min(start + MyMessage.chunkSize, fileBytes.length);
+            byte[] chunkedFileBytes = Arrays.copyOfRange(fileBytes, start, end);
+            System.out.println(chunkedFileBytes.length);
 
-        MyMessage message = new MyMessage(file.getName(), LocalDateTime.now(), mimeType, fileBytes, "sasl-producer", topicName);
-        String jsonString = JsonUtil.mapper().writeValueAsString(message);
-        System.out.println(message);
-        ProducerRecord<String, byte[]> record = new ProducerRecord<>(topicName, jsonString.getBytes(StandardCharsets.UTF_8));
-        fileProducer.send(record, (recordMetadata, e) -> {
-            if (e != null) {
-                log.error("Send failed", e);
-            } else {
-                log.info("File sent");
-            }
-        });
+            MyMessage message = new MyMessage(file.getName(), LocalDateTime.now(), mimeType, chunkedFileBytes, "sasl-producer", topicName,fileId,i,totalChunk);
+
+            message.setFileId(fileId);
+            System.out.println("file id: " + message.getFileId());
+            message.setChunkNumber(i);
+            System.out.println(message.getChunkNumber());
+            message.setTotalChunk(totalChunk);
+            System.out.println(message.getTotalChunk());
+
+            String jsonString = JsonUtil.mapper().writeValueAsString(message);
+            System.out.println(message);
+            ProducerRecord<String, byte[]> record = new ProducerRecord<>(topicName, jsonString.getBytes(StandardCharsets.UTF_8));
+            fileProducer.send(record, (recordMetadata, e) -> {
+                if (e != null) {
+                    log.error("Send failed", e);
+                } else {
+                    log.info("File sent");
+                }
+            });
+        }
     }
 
     public void close() {
